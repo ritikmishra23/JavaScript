58c8dc9efa722f8fba39426cab01941efd5d2f39
Ritik Mishra <ritikmishra23april@gmail.com>
2024-11-04 23:55:47 +0530

    DataTypes in JS.

---
+++ 01_Basics/DataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // it tells that treat all code as newer version.
+
+//alert(2 + 2) // in browser it will run but we are using nodejs so it will show error here.
+
+//console.log("hi") console.log("pi") it will give error use semicolon in between if we are using in same line
+
+console.log(3
+    +
+    3) // code readability should be 
+
+let name = "Ritik"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => if want to store large value.
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique... will use in React to identify unique component
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

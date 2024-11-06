90b8a0e9c546964badd3d63b04db237a318de90f
Ritik Mishra <ritikmishra23april@gmail.com>
2024-11-06 20:52:12 +0530

    Various Mathematical operation practice in JS

---
+++ 01_Basics/Operation.js
@@ -0,0 +1,58 @@
+//Operation in JS
+let value = 5
+let negativeVal = -value;
+//console.log(negativeVal);
+// console.log(2 + 3);   Addition
+// console.log(2 - 3);   Subtraction
+// console.log(2 * 3);   Multiplication
+// console.log(2 ** 3);  Power- 2 raised to 3
+// console.log(2 / 3);    division upto 16 decimal place
+// console.log(2 % 3);   Modulous
+
+let str1 = "Hello"
+let str2 = " Ritik"
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 6)     //16
+console.log(1 + "5");    //15
+
+console.log("1" + 2 + 6)   //126
+console.log(1 + 2 + "2");  //32
+
+console.log(true);    //true
+console.log(+true);   //1
+console.log(+"");     //0
+
+//INCREEMENT And Decreement Operator
+let count = 100;
+console.log(++count);   //101
+console.log(count++);   //101
+console.log(count--);   //102
+console.log(--count);   //100
+
+//COMPARISON OPERATOR
+
+//We can compare different dataType in JavaScript
+console.log("2" > 1); //true
+console.log("02" > 1);//true
+console.log("hello");
+
+
+console.log(null > 0);//false
+console.log(null == 0); //false
+console.log(null >= 0) //true
+
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+
+//Strict equal operator ===
+console.log("2" == 2) //true i.e. it only check value
+console.log("2" === 2);//false i.e. it check value as well as dataType.
+
+
+
+
+
+

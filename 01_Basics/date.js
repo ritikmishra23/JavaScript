9d2776730653f49c803c8236677fe41e259ef070
Ritik Mishra <ritikmishra23april@gmail.com>
2024-11-09 23:23:59 +0530

    date object

---
+++ 01_Basics/date.js
@@ -0,0 +1,12 @@
+// Date objects
+let mydate = new Date();
+console.log(mydate);
+
+//converting date to string
+console.log(mydate.toString()); //Sat Nov 09 2024 10:35:06 GMT+0530 (India Standard time)
+
+console.log(mydate.toDateString()); //Sat Nov 09 2024 
+console.log(mydate.toJSON()) //2024-11-09T17:52:54.918Z
+
+
+

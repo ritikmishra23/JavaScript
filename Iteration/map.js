a11e68c1f609bc0bd3fb687b3064062a8cdba750
Ritik Mishra <ritikmishra23april@gmail.com>
2024-11-24 23:14:03 +0530

    DOM

--- Iteration/map.js
+++ Iteration/map.js
@@ -6,7 +6,7 @@ const a = num.map((val) => val + 10)
 //chaining=> using more function at a time , using map and filter together
 
 let b = num.map((val) => val * 10).map((val) => val + 1)
-console.log(b);
+console.log(b);//[11, 21, 31,41, 51, 61, 71]
 b = num.map((val) => val * 10).map((val) => val + 1).filter((val) => val >= 40)
-console.log(b);
+console.log(b);//[ 41, 51, 61, 71 ]
 
